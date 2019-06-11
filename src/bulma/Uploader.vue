ed7525187170ed9d630127df1e7fa13f90a9b496
Yacoub Belaoura <yacoubbelaoura@gmail.com>
2019-06-11 12:18:59 +0100

    wip tailwind uploader

--- src/bulma/Uploader.vue
+++ src/bulma/Uploader.vue
@@ -3,17 +3,22 @@
         v-on="$listeners"
         ref="uploader">
         <template v-slot:default="{ label, inputBindings, inputEvents, controlEvents }">
-            <form class="is-marginless"
+            <!-- <form class="is-marginless" -->
+            <form class="m-0"
                 @submit.prevent>
-                <input class="is-hidden"
+                <!-- <input class="is-hidden" -->
+                <input class=" hidden"
                     v-bind="inputBindings"
                     v-on="inputEvents">
                 <slot name="control"
                     :control-events="controlEvents">
-                    <a :class="['file', {'is-small': isSmall}, {'is-large': isLarge}]"
+                    <!-- <a :class="['file', {'is-small': isSmall}, {'is-large': isLarge}]" -->
+                    <a :class="['flex items-stretch justify-start relative', {' text-xs': isSmall}, {'text-2xl': isLarge}]"
                         v-on="controlEvents">
-                        <span :class="['file-cta', {'is-rounded': isRounded}]">
-                            <span class="file-icon">
+                        <!-- <span :class="['file-cta', {'is-rounded': isRounded}]"> -->
+                        <span :class="[' bg-white-smoke text-gray-290', {' rounded-full': isRounded}]">
+                            <!-- <span class="file-icon"> -->
+                            <span class=" items-center flex h-3 w-3 ltr:mr-1 rtl:ml-1 justify-center">
                                 <fa icon="upload"/>
                             </span>
                             <span>
@@ -61,9 +66,9 @@ export default {
     },
 };
 </script>
-
-<style lang="scss">
-    .file-cta.is-rounded {
-        border-radius: 290486px;
-    }
-</style>
+// TODO: remove this
+// <style lang="scss">
+//     .file-cta.is-rounded {
+//         border-radius: 290486px;
+//     }
+// </style>
